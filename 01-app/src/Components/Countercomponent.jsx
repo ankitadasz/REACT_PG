58be3ff6c674b6ce010f47ce104d9d862fed0513
Ankita Das <daszankita23@gmail.com>
2025-11-11 19:58:28 +0530

    useState

--- 01-app/src/Components/Countercomponent.jsx
+++ 01-app/src/Components/Countercomponent.jsx
@@ -2,10 +2,10 @@ import React , {useState} from "react";
 
 const Counter = ()=> {
     const [num,setNum]=useState(0);
-    // const [value,setValue]=useState("Even");
     return (
         <div>
             <p>Number-{num}</p>
+            <h6>Number is {num%2===0 ? "EVEN" : "ODD"}</h6>
             <button onClick={()=>setNum(num+1)}>Increment</button>
             <button onClick={()=>setNum(num-1)}>Decrement</button>
             <button>Check</button>
